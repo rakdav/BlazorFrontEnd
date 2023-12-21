a668cb73022e3c124f2783a03055b414a74028d1
Gogs <gogs@fake.local>
2023-12-21 15:09:59 +0200

    Добавьте файлы проекта.

---
+++ BlazorFrontEnd/Models/ClientService.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorFrontEnd.Models
+{
+    public class ClientService
+    {
+        public int IdClient { get; set; }
+        public string? Surname { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string? City { get; set; }
+        public string? Interest { get; set; }
+        public string? Gender { get; set; }
+        public bool HasPair { get; set; }
+    }
+}
