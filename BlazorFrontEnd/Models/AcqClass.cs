6840da3a6d73acdf8cf46284eff7ec2e327e6650
rakdav <rakdav@inbox.ru>
2023-12-23 01:23:54 +0200

    night

---
+++ BlazorFrontEnd/Models/AcqClass.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorFrontEnd.Models
+{
+    public class AcqClass
+    {
+        public int IdAcq { get; set; }
+        public int? IdClientWoman { get; set; }
+        public int? IdClientMan { get; set; }
+        public DateTime? Date { get; set; }
+        public bool WomanAgr { get; set; }
+        public bool ManAgr { get; set; }
+    }
+}
--- BlazorFrontEnd/Models/ClientService.cs
+++ BlazorFrontEnd/Models/ClientService.cs
@@ -1,14 +1,28 @@
-﻿namespace BlazorFrontEnd.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFrontEnd.Models
 {
     public class ClientService
     {
         public int IdClient { get; set; }
+        [Required (ErrorMessage = "Необходимо ввести фамилию")]
+        [StringLength(50)]
         public string? Surname { get; set; }
+        [Required (ErrorMessage = "Необходимо ввести имя")]
+        [StringLength(50)]
         public string? Firstname { get; set; }
         public string? Lastname { get; set; }
+        [Required(ErrorMessage = "Необходимо ввести дату рождения")]
+        [StringLength(50)]
         public DateTime? BirthDate { get; set; }
+        [Required(ErrorMessage = "Необходимо ввести город")]
+        [StringLength(50)]
         public string? City { get; set; }
+        [Required(ErrorMessage = "Необходимо ввести интересы")]
+        [StringLength(50)]
         public string? Interest { get; set; }
+        [Required(ErrorMessage = "Необходимо выбрать ваш пол")]
+        [StringLength(50)]
         public string? Gender { get; set; }
         public bool HasPair { get; set; }
     }
